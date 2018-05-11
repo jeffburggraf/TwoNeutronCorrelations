a976467b2572efd7e5e7699463e3b9c2f455da84
jeffreyburggraf <jeffburggraf@gmail.com>
2018-05-11 16:05:04 -0600

    nothing

--- plots/PoissonVerification.py
+++ plots/PoissonVerification.py
@@ -1,13 +1,26 @@
 import ROOT
 import numpy as np
+import mytools2 as mt2
 
 
 
+treeSP_doubles, _ = mt2.NCorrRun("SP", "DU", generate_dictionary=False, Forward=True).neutrons_doubles_tree
+
+treeSP_singles, pulses_SP_singles = mt2.NCorrRun("SP", "DU", generate_dictionary=False, Forward=True).neutrons_singles_tree
+
+treeDP_doubles, pulses_DP_doubles = mt2.NCorrRun("DP", "DU", generate_dictionary=False, Forward=True).neutrons_doubles_tree
 
 
+zero_hits = (pulses_SP_singles-treeSP_singles.GetEntries() - treeSP_doubles.GetEntries())/pulses_SP_singles
+zero_hits_err = np.sqrt(pulses_SP_singles-treeSP_singles.GetEntries() - treeSP_doubles.GetEntries())/pulses_SP_singles
 
+single_hits = (treeSP_singles.GetEntries())/pulses_SP_singles
+single_hits_err = np.sqrt(treeSP_singles.GetEntries())/pulses_SP_singles
 
+doubles_hits = treeSP_doubles.GetEntries() / pulses_DP_doubles
+doubles_hits_err = np.sqrt(treeSP_doubles.GetEntries()) / pulses_DP_doubles
 
+print (zero_hits, single_hits, doubles_hits)
 
 
 if __name__ == "__main__":
