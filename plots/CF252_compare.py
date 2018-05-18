bf73fd9a6d825539274701e1e51238e6d6835284
jeffreyburggraf <jeffburggraf@gmail.com>
2018-05-18 15:28:35 -0600

    nothing

---
+++ plots/CF252_compare.py
@@ -0,0 +1,48 @@
+import ROOT
+import numpy as np
+import mytools2 as mt2
+import re
+from TH1Wrapper import *
+
+
+
+
+
+if __name__ == "__main__":
+    import ROOT as ROOT
+    from multiprocessing import Process, Queue
+    import time, sys, os
+
+
+    def input_thread(q, stdin):
+        while True:
+            print 'ROOT: '
+            cmd = stdin.readline()
+            q.put(cmd)
+
+
+    def root(char):
+        assert isinstance(char, str), "Argument must be string!"
+        ROOT.gROOT.ProcessLine(char)
+
+
+    if __name__ == '__main__':
+        ___queue___ = Queue()
+        ___newstdin___ = os.fdopen(os.dup(sys.stdin.fileno()))
+        ___input_p___ = Process(target=input_thread,
+                                args=(___queue___, ___newstdin___))
+        ___input_p___.daemon = True
+        ___input_p___.start()
+        ___g___ = ROOT.gSystem.ProcessEvents
+        try:
+            while 1:
+                if not ___queue___.empty():
+                    ___cmd___ = ___queue___.get()
+                    try:
+                        exec (___cmd___, globals())
+                    except:
+                        print sys.exc_info()
+                time.sleep(0.01)
+                ___g___()
+        except(KeyboardInterrupt, SystemExit):
+            ___input_p___.terminate()
