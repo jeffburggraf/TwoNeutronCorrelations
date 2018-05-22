421e594877383220107c33865f487b49cf0c41a4
jeffreyburggraf <jeffburggraf@gmail.com>
2018-05-21 22:14:18 -0600

    nothing

--- plots/theta_absRel.py
+++ plots/theta_absRel.py
@@ -1,29 +1,48 @@
 import ROOT
 import numpy as np
-from TH1Wrapper import*
 import mytools2 as mt2
+from TH1Wrapper import TH1F
+import mytools as mt
 
-import ROOT
-import numpy as np
-import mytools2 as mt2
+targets = "DU", 'Pu'
 
-target = "DU"
+# subtract = 'U233'
 
-# treeSP_doubles, pulses_SP_doubles = mt2.NCorrRun("SP", target, generate_dictionary=False, Forward=True).neutrons_doubles_tree
-#
+treeSP1, pulses_SP1_doubles = mt2.NCorrRun("SP", targets[0], generate_dictionary=False, Forward=True).neutrons_doubles_tree
+treeSP2, pulses_SP2_doubles = mt2.NCorrRun("SP", targets[-1], generate_dictionary=False,Forward=True).neutrons_doubles_tree
 
-hist = TH1F(9-10,9+10,binwidths=0.5)
 
-for i in np.random.randn(20000):
-    hist.Fill(i*3 + 9)
+# erg_bins =mt2.median(D2OErgHist, 4)[0]
+# if erg_bins[0]<0.5:
+#     erg_bins[0] = 0.5
 
-hist.GetXaxis().SetTitle("PMT difference")
-hist.GetYaxis().SetTitle("counts")
-hist.Draw()
-ROOT.gStyle.SetOptStat('rm')
-hist.SetTitle('')
+# dx_pos = (30*2.5)/3.
+# binszpos = np.arange(-2.5*15, 2.5*15 +dx_pos, dx_pos , dtype=np.float32)
 
-mt2.thesis_plot(hist)
+binsphi = []
+for degree in mt.angles:
+    binsphi.append(degree - 10)
+    binsphi.append(degree + 10)
+    if degree>180:break
+
+binsphi = [0] + binsphi
+
+hist1 = TH1F(binarrays=binsphi)
+hist2 = TH1F(binarrays=binsphi)
+
+hist1.Project(treeSP1, '180/3.1415*neutrons.coinc_hits.theta_abs')
+hist2.Project(treeSP2, '180/3.1415*neutrons.coinc_hits.theta_abs')
+
+
+hist1 /= hist2
+
+hist1.SetTitle('{0}/{1}'.format(*targets))
+hist1.GetXaxis().SetTitle("#theta_{abs}")
+hist1.GetYaxis().SetTitle("ratio")
+
+hist1.Draw()
+mt2.thesis_plot(hist1)
+
 
 if __name__ == "__main__":
     import ROOT as ROOT
@@ -33,7 +52,7 @@ if __name__ == "__main__":
 
     def input_thread(q, stdin):
         while True:
-            print ('ROOT: ')
+            print 'ROOT: '
             cmd = stdin.readline()
             q.put(cmd)
 
@@ -58,8 +77,10 @@ if __name__ == "__main__":
                     try:
                         exec (___cmd___, globals())
                     except:
-                        print(sys.exc_info())
+                        print sys.exc_info()
                 time.sleep(0.01)
                 ___g___()
         except(KeyboardInterrupt, SystemExit):
             ___input_p___.terminate()
+
+
